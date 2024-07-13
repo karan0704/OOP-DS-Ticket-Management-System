cba6967cdb03d0049062604892a0d71d96d7d344
Karan Mohokar <karanmohokar7@gmail.com>
2024-07-14 00:53:24 +0530

    Added Service Interface and createTicket

---
+++ src/service/TicketService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package service;
+
+import model.Ticket;
+import model.TicketStatus;
+
+/**
+ * @author Karan
+ *
+ */
+public interface TicketService {
+
+	void createTIcket(Ticket ticket);
+	/*
+	 * Ticket getTicketbyID(int id); boolean changeTicketStatus(int id, TicketStatus
+	 * Status); void deleteTicketbyID(int id);
+	 */
+
+
+}
