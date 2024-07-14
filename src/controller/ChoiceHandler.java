af79d9d5dc3aba8f1165bea2e39c437e7fb6e28e
Karan Mohokar <karanmohokar7@gmail.com>
2024-07-14 11:46:11 +0530

    Developed ChoiceHandler class for user interactions

---
+++ src/controller/ChoiceHandler.java
@@ -0,0 +1,10 @@
+package controller;
+
+import service.TicketService;
+import service.TicketServiceImpl;
+
+public class ChoiceHandler {
+    private TicketService ticketService = new TicketServiceImpl();
+
+ 
+}
